8876e915994329402cb3a153b31ff692f2fe3344
Ilya,golang <ilya.bolotov2013@yandex.ru>
2024-06-27 16:47:26 +0500

    new items catalog

--- src/components/ItemInfo.js
+++ src/components/ItemInfo.js
@@ -1,4 +1,6 @@
 import React from 'react';
+import ShoppingCartIcon from '@mui/icons-material/ShoppingCart';
+import FavoriteIcon from '@mui/icons-material/Favorite';
 // import { useNavigate } from 'react-router-dom';
 export default function ItemInfo({item}){
     // const navigate = useNavigate();
@@ -9,20 +11,34 @@ export default function ItemInfo({item}){
     // };
     return(
         <>
-        <div className='row item-container'>
+        <div className='item-container'>
+            <div className='row item-container2'>
             <div className='col-6 item-text'>
                 
                 <h3>{item.name}</h3>
            
             </div>
+            <hr className="vertical-divider" />
             <div className='col-3 '>
-            <p>{item.price} ₽</p>
+            <p className='item-price'>{item.price} ₽</p>
             </div>
+            <hr className="vertical-divider" />
             <div className='col-3 '>
-                <p>{item.count} шт</p>
+                <p className='item-count'>{item.count} шт</p>
+            </div>
+            </div>
+            <div className='buy-save-container'>
+            <div className='buy-button'>
+                <ShoppingCartIcon/>
+                <h3 className='Text-buy-button'>Купить</h3>
+            </div>
+            <div className='buy-button'>
+            <FavoriteIcon/>
+                <h3 className='Text-buy-button'>В избранное</h3>
+            </div>
             </div>
         </div>
-           
+        
         </>
     )
 }
--- src/components/ItemsPage.js
+++ src/components/ItemsPage.js
@@ -1,28 +1,24 @@
 import { useParams } from 'react-router-dom';
 import React, { useState, useEffect } from 'react';
 import Spinner from './SpinnerLoader';
-import Items from './Items'
+import Items from './Items';
 import Footer from './Footer';
-import Header from './Header'
+import Header from './Header';
 
-export default function ItemsPage(){
-    const { id } = useParams();
-    const [loading, setLoading] = useState(true);
-    const [data, setData] = useState(null);
-    const fakeData = 
-   [
-    {id: 1, categoryId: 1, name: "Super-Fast-vpn++dafaaffnajfanfanfnajjfnanалфафофовфоаофалоалфоа", price:250, description:"Хороший впн", count: 2},
-    {id: 2, categoryId: 1, name: "Hello world", price:260, description:"Хороший впн", count: 2},
-    {id: 3, categoryId: 1, name: "FastVpn", price:220, description:"Хороший впн", count: 2},
-    {id: 4, categoryId: 1, name: "AstraBpn", price:280, description:"Хороший впн", count: 2},
-
-   ]
+export default function ItemsPage() {
+  const { id } = useParams();
+  const [loading, setLoading] = useState(true);
+  const [data, setData] = useState(null);
+  const fakeData = [
+    { id: 1, categoryId: 1, name: "Super-Fast-vpn++dafaaffnajfanfanfnajjfnanалфафофовфоаофалоалфоа", price: 250, description: "Хороший впн", count: 2 },
+    { id: 2, categoryId: 1, name: "Hello world", price: 260, description: "Хороший впн", count: 2 },
+    { id: 3, categoryId: 1, name: "FastVpn", price: 220, description: "Хороший впн", count: 2 },
+    { id: 4, categoryId: 1, name: "AstraBpn", price: 280, description: "Хороший впн", count: 2 },
+    { id: 4, categoryId: 2, name: "AstraProxy", price: 280, description: "Хороший прокси", count: 2 },
+  ];
   
-  
-  
-
-// Функция для получения данных
-const fetchData = async () => {
+  // Функция для получения данных
+  const fetchData = async () => {
     try {
       // Искусственная задержка для имитации сетевого запроса
       await new Promise((resolve) => setTimeout(resolve, 500));
@@ -34,31 +30,21 @@ const fetchData = async () => {
     }
   };
 
-//  // Функция для получения данных
-//  const fetchData = async () => {
-//    try {
-//      const response = await fetch('https://api.example.com/data'); // Путь к вашему API
-//      const result = await response.json();
-//      setData(result);
-//    } catch (error) {
-//      console.error('Ошибка при загрузке данных:', error);
-//    } finally {
-//      setLoading(false);
-//    }
-//  };
-
   useEffect(() => {
     fetchData(); // Загружаем данные при монтировании компонента
   }, []);
 
+  const filteredData = data ? data.filter(item => item.categoryId === parseInt(id)) : [];
+
   return (
     <>
-      {loading ? <Spinner /> : <div>
-      <Header/>
-      <Items data={(data)}/>
-      <Footer active={'active'}/>
-      </div>}
-      
+      {loading ? <Spinner /> : 
+        <div>
+          <Header />
+          <Items data={filteredData} />
+          <Footer active='active' />
+        </div>
+      }
     </>
   );
-};
+}
